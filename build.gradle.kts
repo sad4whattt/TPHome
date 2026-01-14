b5104a3e1d6485dece6f11c19c3c9e9c1e6ef17a
sad4whattt <violetfroaway@proton.me>
2026-01-13 20:10:03 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "com.snipr"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Hytale Server API
+    compileOnly(files("libs/HytaleServer.jar"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        archiveBaseName.set("HomePlugin")
+    }
+    
+    build {
+        dependsOn(shadowJar)
+    }
+}
